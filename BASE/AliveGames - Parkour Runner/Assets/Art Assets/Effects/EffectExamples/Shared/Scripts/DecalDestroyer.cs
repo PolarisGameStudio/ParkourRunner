59031825efa108c1d3bc6c4f9d9e064a86a5d3ce
AlmanInc <sevhwarang@gmail.com>
2019-10-03 12:38:26 +0300

    Temporary restore effects for mine

---
+++ BASE/AliveGames - Parkour Runner/Assets/Art Assets/Effects/EffectExamples/Shared/Scripts/DecalDestroyer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace EffectExamples.Shared.Scripts
+{
+    public class DecalDestroyer : MonoBehaviour {
+
+        public float lifeTime = 5.0f;
+
+        private IEnumerator Start()
+        {
+            yield return new WaitForSeconds(lifeTime);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ BASE/AliveGames - Parkour Runner/Assets/Art Assets/Effects/EffectExamples/Shared/Scripts/ParticleExamples.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace EffectExamples.Shared.Scripts
+{
+    [System.Serializable]
+    public class ParticleExamples {
+
+        public string title;
+        [TextArea]
+        public string description;
+        public bool isWeaponEffect;
+        public GameObject particleSystemGO;
+        public Vector3 particlePosition, particleRotation;
+    }
+}
