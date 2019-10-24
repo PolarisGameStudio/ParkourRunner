2852a25826c4cc005da78d03507f8a9489c2243c
AlmanInc <sevhwarang@gmail.com>
2019-10-24 13:58:59 +0300

    Last fixes (quest)

--- BASE/AliveGames - Parkour Runner/Assets/Scripts/Menu/Quests/QuestMenu.cs
+++ BASE/AliveGames - Parkour Runner/Assets/Scripts/Menu/Quests/QuestMenu.cs
@@ -7,6 +7,7 @@ public class QuestMenu : Menu
 {
     [Header("Animation settings")]
     [SerializeField] private MovingAnimation _backButtonAnim;
+    [SerializeField] private MovingAnimation _captionAnim;
     [SerializeField] private AlphaAnimation _levelsAnim;
 
     protected override void Show()
@@ -16,6 +17,7 @@ public class QuestMenu : Menu
         var secuance = DOTween.Sequence();
         secuance.Append(_levelsAnim.Show());
         secuance.Insert(0.1f, _backButtonAnim.Show());
+        secuance.Insert(0.1f, _captionAnim.Show());
     }
 
     protected override void StartHide(Action callback)
@@ -24,6 +26,7 @@ public class QuestMenu : Menu
 
         var secuance = DOTween.Sequence();
         secuance.Append(_backButtonAnim.Hide());
+        secuance.Insert(0f, _captionAnim.Hide());
         secuance.Insert(0.2f, _levelsAnim.Hide());
 
         secuance.OnComplete(() =>
