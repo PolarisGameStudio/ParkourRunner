ce52c9f68dfe73cea143b1998c05678fcfb4ba82
AlmanInc <sevhwarang@gmail.com>
2019-10-27 12:40:31 +0300

    Last fixes

---
+++ BASE/AliveGames - Parkour Runner/Assets/Scripts/Quests/DailyQuestController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+
+public class DailyQuestController : MonoBehaviour
+{
+    [SerializeField] private List<QuestItem> _quests;
+    [SerializeField] private int _activeQuestsCount;
+
+    private List<QuestItem> _activeQuests;
+
+    private void OnEnable()
+    {
+        SelectRandomQuests();
+        ShowActiveQuests();
+    }
+
+    private void CheckTime()
+    {
+        var date = DateTime.Now;
+        
+        
+    }
+
+    private void SelectRandomQuests()
+    {
+        if (_activeQuests != null)
+            _activeQuests.Clear();
+        else
+            _activeQuests = new List<QuestItem>();
+
+        List<QuestItem> list = new List<QuestItem>(_quests);
+
+        for (int i = 0; i < _activeQuestsCount; i++)
+        {
+            QuestItem item = list[UnityEngine.Random.Range(0, list.Count)];
+            _activeQuests.Add(item);
+
+            list = list.Where(x => x.IsEnable && x.ID != item.ID).ToList();
+        }
+    }
+
+    private void ShowActiveQuests()
+    {
+        foreach (var item in _quests)
+            item.gameObject.SetActive(false);
+
+        foreach (var item in _activeQuests)
+            item.gameObject.SetActive(true);
+    }
+}
---
+++ BASE/AliveGames - Parkour Runner/Assets/Scripts/Quests/QuestItem.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class QuestItem : MonoBehaviour
+{
+    [SerializeField] private int _id;
+    [SerializeField] private bool _isEnable;
+
+    public int ID { get { return _id; } }
+
+    public bool IsEnable { get { return _isEnable; } }
+}
