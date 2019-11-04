fdcc92c9be79e9e4bc1ad8373aaa21868542111e
AlmanInc <sevhwarang@gmail.com>
2019-11-04 11:29:27 +0300

    URL mini-fix (Rate me)

--- BASE/AliveGames - Parkour Runner/Assets/Scripts/MainMenuAndShop/Settings/GiveLike.cs
+++ BASE/AliveGames - Parkour Runner/Assets/Scripts/MainMenuAndShop/Settings/GiveLike.cs
@@ -7,7 +7,11 @@ public class GiveLike : SettingsBase
     public override void OnClick()
     {
         AudioManager.Instance.PlaySound(Sounds.Tap);
+        OpenStore();
+    }
 
+    public static void OpenStore()
+    {
 #if UNITY_IPHONE || UNITY_IOS
         Application.OpenURL(StaticConst.IOS_URL);
 #elif UNITY_ANDROID
