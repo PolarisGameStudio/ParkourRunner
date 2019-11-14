648b82a5f177c0afef885b1704f2d185070a3365
AlmanInc <sevhwarang@gmail.com>
2019-11-14 15:23:26 +0300

    Appodeal Integration 3

---
+++ BASE/AliveGames - Parkour Runner/Assets/Editor/CFacebookPostProcessBuild.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using UnityEditor.iOS.Xcode;
+using UnityEditor.Callbacks;
+ 
+namespace UnityEditor.CFacebook
+{
+	public static class CFacebookPostProcessBuild
+    {
+		const string FACEBOOK_FRAMEWORKS_FOLDER = "$(PROJECT_DIR)/Frameworks/Plugins/iOS/Facebook";
+
+		[PostProcessBuild(99999999)]
+		public static void OnPostProcessBuild(BuildTarget buildTarget, string path)
+		{
+            if (buildTarget == BuildTarget.iOS || buildTarget == BuildTarget.iOS)
+			{
+				string projectPath = path+"/Unity-iPhone.xcodeproj/project.pbxproj";
+
+				// Create a new project object from build target
+				PBXProject project = new PBXProject();
+				var file = File.ReadAllText(projectPath);
+				project.ReadFromString(file);
+
+				string target = project.TargetGuidByName("Unity-iPhone");
+
+				project.AddBuildProperty(target, "FRAMEWORK_SEARCH_PATHS", FACEBOOK_FRAMEWORKS_FOLDER);
+				
+				project.SetBuildProperty(target, "ENABLE_BITCODE", "NO");
+
+				// Finally save the xcode project
+				File.WriteAllText(projectPath, project.WriteToString());
+			}
+        }
+	}
+}
