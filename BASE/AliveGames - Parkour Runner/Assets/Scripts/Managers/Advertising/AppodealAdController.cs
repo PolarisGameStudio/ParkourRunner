ed5b8b3c4d08d15274be2c56d01c096aba184a17
anonim22 <maxiaka229@gmail.com>
2019-12-17 12:21:42 +0500

    Appodeal fix

--- BASE/AliveGames - Parkour Runner/Assets/Scripts/Managers/Advertising/AppodealAdController.cs
+++ BASE/AliveGames - Parkour Runner/Assets/Scripts/Managers/Advertising/AppodealAdController.cs
@@ -3,78 +3,78 @@ using AppodealAds.Unity.Api;
 using AppodealAds.Unity.Common;
 using UnityEngine;
 
-public class AppodealAdController : BaseAdController, IInterstitialAdListener
-{
-    private const int MAX_FRAMES_TO_INTERSTITIAL = 2;
+public class AppodealAdController : BaseAdController, IInterstitialAdListener {
+	private const int MAX_FRAMES_TO_INTERSTITIAL = 2;
 
-    [SerializeField] private string _androidAppKey;
-    [SerializeField] private string _iosAppKey;
-    [SerializeField] private bool _isTesting;
-    
-    public override void Initialize()
-    {
-        Appodeal.setTesting(_isTesting);
-        
+	[SerializeField] private string _androidAppKey;
+	[SerializeField] private string _iosAppKey;
+	[SerializeField] private bool   _isTesting;
+
+	private bool _failed, _success, _skipped;
+
+
+	private void Update() {
+		if (_failed) HandleAdResult(AdResults.Failed);
+		if (_success) HandleAdResult(AdResults.Finished);
+		if (_skipped) HandleAdResult(AdResults.Skipped);
+
+		_failed = _success = _skipped = false;
+	}
+
+
+	public override void Initialize() {
+		Appodeal.setTesting(_isTesting);
+
 #if UNITY_IPHONE || UNITY_IOS
-        //Appodeal.initialize(_iosAppKey, Appodeal.NON_SKIPPABLE_VIDEO | Appodeal.INTERSTITIAL);
         Appodeal.initialize(_iosAppKey, Appodeal.INTERSTITIAL);
 #elif UNITY_ANDROID
-        Appodeal.initialize(_androidAppKey, Appodeal.INTERSTITIAL);
+		Appodeal.initialize(_androidAppKey, Appodeal.INTERSTITIAL);
 #endif
 
-        Appodeal.setInterstitialCallbacks(this);
-    }
+		Appodeal.setInterstitialCallbacks(this);
+	}
 
-    public override bool IsAvailable()
-    {
-        return true;
-    }
-        
-    public override void ShowAdvertising()
-    {
-        StartCoroutine(ShowAdsProcess());
 
-        //if (Appodeal.isLoaded(Appodeal.NON_SKIPPABLE_VIDEO))
-        //    Appodeal.show(Appodeal.NON_SKIPPABLE_VIDEO);
-        //else
-        //    Appodeal.show(Appodeal.INTERSTITIAL);
-    }
+	public override bool IsAvailable() {
+		return true;
+	}
 
-    private IEnumerator ShowAdsProcess()
-    {
-        yield return new WaitWhile(() => !Appodeal.isLoaded(Appodeal.INTERSTITIAL));
 
-        Appodeal.show(Appodeal.INTERSTITIAL);
-    }
+	public override void ShowAdvertising() {
+		StartCoroutine(ShowAdsProcess());
+	}
 
-    #region Interface
-    // IInterstitialAdListener
-    public void onInterstitialFailedToLoad()
-    {
-        //HandleAdResult(UnityEngine.Advertisements.ShowResult.Failed);
-        HandleAdResult(AdResults.Failed);
-    }
 
-    public void onInterstitialExpired()
-    {
-        //HandleAdResult(UnityEngine.Advertisements.ShowResult.Failed);
-        HandleAdResult(AdResults.Failed);
-    }
+	private IEnumerator ShowAdsProcess() {
+		yield return new WaitWhile(() => !Appodeal.isLoaded(Appodeal.INTERSTITIAL));
 
-    public void onInterstitialLoaded(bool isPrecache) { }
+		Appodeal.show(Appodeal.INTERSTITIAL);
+	}
 
-    public void onInterstitialClicked() { }
 
-    public void onInterstitialClosed()
-    {
-        //HandleAdResult(UnityEngine.Advertisements.ShowResult.Finished);
-        HandleAdResult(AdResults.Finished);
-    }
+	#region Interface
 
-    public void onInterstitialShown()
-    {
-        //HandleAdResult(UnityEngine.Advertisements.ShowResult.Finished);
-        HandleAdResult(AdResults.Finished);
-    }
-    #endregion
+	public void onInterstitialFailedToLoad() {
+		_failed = true;
+	}
+
+
+	public void onInterstitialExpired() {
+		_failed = true;
+	}
+
+
+	public void onInterstitialLoaded(bool isPrecache) { }
+
+	public void onInterstitialClicked() { }
+
+
+	public void onInterstitialClosed() {
+		_success = true;
+	}
+
+
+	public void onInterstitialShown() { }
+
+	#endregion
 }
