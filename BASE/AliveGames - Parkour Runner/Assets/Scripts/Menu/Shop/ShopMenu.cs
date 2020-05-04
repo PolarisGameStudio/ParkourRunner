a8680b23ae422be699c6691477d131169872ef88
anonim22 <maxiaka229@gmail.com>
2020-05-04 14:07:10 +0500

    Переделан магазин с заделом на будущие шлемы. Добавлена кнопка восстановления покупок(NoAds) для IOS

--- BASE/AliveGames - Parkour Runner/Assets/Scripts/Menu/Shop/ShopMenu.cs
+++ BASE/AliveGames - Parkour Runner/Assets/Scripts/Menu/Shop/ShopMenu.cs
@@ -3,56 +3,69 @@ using UnityEngine;
 using DG.Tweening;
 using AEngine;
 
-public class ShopMenu : Menu
-{
-    public static event Action OnShowMenu;
-    public static event Action OnHideMenu;
+public class ShopMenu : Menu {
+	public static event Action OnShowMenu;
+	public static event Action OnHideMenu;
 
-    [Header("Animation settings")]
-    [SerializeField] private float _showAfterBgDelay;
-    [SerializeField] private float _hideForBgDelay;
-    [SerializeField] private MovingAnimation _homeButtonAnim;
-    [SerializeField] private MovingAnimation _playerStatusAnim;
-    [SerializeField] private MovingAnimation _shopAnim;
-    [SerializeField] private AlphaAnimation _backgroundAnim;    
+	[Header("Animation settings")] [SerializeField]
+	private float _showAfterBgDelay;
+	[SerializeField] private float           _hideForBgDelay;
+	[SerializeField] private MovingAnimation _homeButtonAnim;
+	[SerializeField] private MovingAnimation _playerStatusAnim;
+	[SerializeField] private MovingAnimation _shopAnim;
+	[SerializeField] private AlphaAnimation  _backgroundAnim;
 
-    protected override void Show()
-    {
-        base.Show();
-        OnShowMenu.SafeInvoke();
+	[Space] [SerializeField] private GameObject RestoreButton;
 
-        var secuance = DOTween.Sequence();
-        secuance.Append(_backgroundAnim.Hide());
 
-        secuance.Insert(_showAfterBgDelay, _shopAnim.Show());
-        secuance.Insert(_showAfterBgDelay, _playerStatusAnim.Show());
-        secuance.Insert(_showAfterBgDelay, _homeButtonAnim.Show());
-    }
+	private void Start() {
+		if (Application.platform != RuntimePlatform.IPhonePlayer &&
+			Application.platform != RuntimePlatform.OSXPlayer) {
+			RestoreButton.SetActive(false);
+		}
+	}
 
-    protected override void StartHide(Action callback)
-    {
-        base.StartHide(callback);
-        OnHideMenu.SafeInvoke();
 
-        var secuance = DOTween.Sequence();
+	protected override void Show() {
+		base.Show();
+		OnShowMenu.SafeInvoke();
 
-        secuance.Append(_shopAnim.Hide());
-        
-        secuance.Insert(0f, _playerStatusAnim.Hide());
-        secuance.Insert(0f, _homeButtonAnim.Hide());
-        secuance.Insert(_hideForBgDelay, _backgroundAnim.Show());
-        
-        secuance.OnComplete(() =>
-        {
-            FinishHide(callback);
-        });
-    }
+		var secuance = DOTween.Sequence();
+		secuance.Append(_backgroundAnim.Hide());
 
-    #region Events
-    public void OnHomeButtonClick()
-    {
-        _audio.PlaySound(Sounds.Tap);
-        _menuController.OpenMenu(MenuKinds.MainMenu);
-    }
-    #endregion
+		secuance.Insert(_showAfterBgDelay, _shopAnim.Show());
+		secuance.Insert(_showAfterBgDelay, _playerStatusAnim.Show());
+		secuance.Insert(_showAfterBgDelay, _homeButtonAnim.Show());
+	}
+
+
+	protected override void StartHide(Action callback) {
+		base.StartHide(callback);
+		OnHideMenu.SafeInvoke();
+
+		var secuance = DOTween.Sequence();
+
+		secuance.Append(_shopAnim.Hide());
+
+		secuance.Insert(0f,              _playerStatusAnim.Hide());
+		secuance.Insert(0f,              _homeButtonAnim.Hide());
+		secuance.Insert(_hideForBgDelay, _backgroundAnim.Show());
+
+		secuance.OnComplete(() => { FinishHide(callback); });
+	}
+
+
+	#region Events
+
+	public void OnHomeButtonClick() {
+		_audio.PlaySound(Sounds.Tap);
+		_menuController.OpenMenu(MenuKinds.MainMenu);
+	}
+
+
+	public void OnRestoreButton() {
+		InAppManager.Instance.RestorePurchases();
+	}
+
+	#endregion
 }
